70bed8dd50e7c23eff7941aea4db15afb3623221
Joseph Holdnak <90223594+jholdnak@users.noreply.github.com>
2024-11-20 16:43:21 -0800

    Part way completion of conversion to AngleSharp

--- ShapeHandler/ShapeDetector.cs
+++ ShapeHandler/ShapeDetector.cs
@@ -14,6 +14,8 @@ using System.Runtime.Remoting.Contexts;
 using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using Microsoft.Extensions.Azure;
 
 namespace ShapeHandler
 {
@@ -32,26 +34,38 @@ namespace ShapeHandler
         public static HtmlGraph CreateGraphFromFlowchart(Visio.Application application)
         {
             Page page = application.ActivePage;
-            IDictionary<string, IDictionary<string, string>> shapesFromVisio = FetchShapesFromVisio(page.Shapes);
+            List<VisioShape> shapesFromVisio = FetchShapesFromVisio(page.Shapes);
             HtmlGraph graph = new HtmlGraph();
             // TODO: Implement
             return null;
         }
 
+        class VisioShape
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public IDictionary<string, string> ShapeData { get; set; }
+        }
+
         /// <summary>
         /// Intakes shapes from Visio
         /// </summary>
-        /// <param name="shapes">Visio shapes</param>
+        /// <param name="shapesFromVisio">Visio shapes</param>
         /// <returns>Inner dictionary with shapes and their properties</returns>
-        private static IDictionary<string, IDictionary<string, string>> FetchShapesFromVisio(Shapes shapes)
+        private static List<VisioShape> FetchShapesFromVisio(Shapes shapesFromVisio)
         {
-            IDictionary<string, IDictionary<string, string>> shapeData = new Dictionary<string, IDictionary<string, string>>();
-            foreach (Shape shape in shapes)
+            List<VisioShape> shapes = new List<VisioShape>();
+            foreach (Shape shapeFromVisio in shapesFromVisio)
             {
-                IDictionary<string, string> properties = ReadShapeData(shape);
-                shapeData[shape.Text] = properties;
+                VisioShape shape = new VisioShape
+                {
+                    Name = shapeFromVisio.Text,
+                    Type = shapeFromVisio.Name,
+                    ShapeData = ReadShapeData(shapeFromVisio)
+                };
+                shapes.Add(shape);
             }
-            return shapeData;
+            return shapes;
         }
 
         /// <summary>
@@ -83,6 +97,116 @@ namespace ShapeHandler
                 iRow++;
             }
             return properties;
+        }
+
+        private static void ConvertShapesToAngleSharp(List<VisioShape> shapes)
+        {
+            HtmlGraph htmlGraph = new HtmlGraph();
+            IBrowsingContext context = BrowsingContext.New(Configuration.Default);
+            IDocument document = context.OpenNewAsync().Result;
+            DataInputNode viewRelatedDataFormNode = new DataInputNode("View Related Data");
+            DecisionNode viewRelatedDataDecisionNode = new DecisionNode("View Related Data?");
+            foreach (VisioShape shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case "Start/End":
+                        {
+                            StartEndNode startEnd = null;
+                            if (shape.Name == "start")
+                            {
+                                startEnd = new StartEndNode("start")
+                                {
+                                    IsStart = true
+                                };
+                            }
+                            if (shape.Name == "end")
+                                startEnd = new StartEndNode("end");
+                            htmlGraph.AddNode(startEnd);
+                        }
+                        break;
+                    case "Decision":
+                        {
+                            // TODO: Change the properties
+                            // to be more generalized and
+                            // not based off the example
+                            dynamic button;
+                            HtmlNode buttonNode;
+                            if ("lol" == "lol") // submit
+                            {
+                                button = document.CreateElement("input") as IHtmlInputElement;
+                                button.Id = "submitButton";
+                                button.Type = "submit";
+                                buttonNode = new HtmlNode(button.Id, button, Objects.NodeType.Button);
+                            }
+                            if ("nice" == "nice") // cancel
+                            {
+                                button = document.CreateElement("a") as IHtmlAnchorElement;
+                                button.Id = "cancelButton";
+                                button.Href = "/fast-cooling/input";
+                                buttonNode = new HtmlNode(button.Id, button, Objects.NodeType.Anchor);
+                            }
+                            if ("yes" == "yes") // yes button for view related form data?
+                            {
+                                button = document.CreateElement("button") as IHtmlInputElement;
+                                button.Id = "viewRelatedDataButton";
+                                button.Type = "submit";
+                                buttonNode = new HtmlNode(button.Id, button, Objects.NodeType.Button);
+                            }
+                            if ("no" == "no") // no button for view related form data?
+                            {
+                                button = document.CreateElement("button") as IHtmlAnchorElement;
+                                button.Type = "submit";
+                                buttonNode = new HtmlNode(button.Id, button, Objects.NodeType.Anchor);
+                            }
+                            if ("backToInput" == "backToInput") // back to input for view related form data?
+                            {
+                                button = document.CreateElement("a") as IHtmlAnchorElement;
+                                button.Href = "/fast-cooling/input";
+                                button.ClassList.Add(new string[] { "btn", "btn-primary" });
+                            }
+                            viewRelatedDataFormNode.DataInputNodes.Add(buttonNode);
+                            htmlGraph.AddNode(buttonNode);
+                        }
+                        break;
+                    case "Input Data":
+                        {
+                            // TODO: Change the properties
+                            // to be more generalized and
+                            // not based off the example
+                            IHtmlInputElement input = document.CreateElement("input") as IHtmlInputElement;
+                            input.Type = "number";
+                            input.Id = "inputTempSensor";
+                            input.Minimum = 50.ToString();
+                            input.Maximum = 150.ToString();
+                            input.ClassList.Add("form-control");
+                            HtmlNode inputNode = new HtmlNode(input.Id, input, Objects.NodeType.Input);
+                            htmlGraph.AddNode(inputNode);
+                        }
+                        break;
+                    case "foreground process":
+                        {
+                            // TODO: Put foreground process stuff
+                        }
+                        break;
+                    case "Page":
+                        {
+                            PageNode pageANode = new PageNode("A");
+                            htmlGraph.AddNode(pageANode);
+                        }
+                        break;
+                    case "Special connector":
+                        {
+                            // TODO: Put special connector
+                        }
+                        break;
+                    case "Background process":
+                        {
+                            // TODO: Put background process connector
+                        }
+                        break;
+                }
+            }
         }
 
         #region VSTO generated code
