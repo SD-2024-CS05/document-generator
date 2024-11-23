ecd7d6bec3cc839f121f7bbfe7a8d0bb80cc850c
Sebastian Matthews <smatthews2003@protonmail.com>
2024-11-22 21:22:06 -0800

    added eventlistener upon drop shape event

---
+++ ShapeHandler/Database/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeHandler.Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //this.Application.ActiveDocument.ShapeAdded += new Microsoft.Office.Interop.Visio.EDocument_ShapeAddedEventHandler(ActiveDocument_ShapeAdded);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- ShapeHandler/ShapeDetector.cs
+++ ShapeHandler/ShapeDetector.cs
@@ -14,14 +14,33 @@ namespace ShapeHandler
 {
     public partial class ShapeDetector
     {
+        private Form1 fForm = new Form1();
         private void ShapeDetector_Startup(object sender, System.EventArgs e)
         {
             // Uncomment for local testing
             //ShapeDetector.CreateGraphFromFlowchart(Globals.ShapeDetector.Application);
+            this.Application.DocumentOpened += new Visio.EApplication_DocumentOpenedEventHandler(Application_DocumentOpened);
+        }
+
+        private void Application_DocumentOpened(Visio.Document doc)
+        {
+            this.Application.ActiveDocument.ShapeAdded += new Microsoft.Office.Interop.Visio.EDocument_ShapeAddedEventHandler(ActiveDocument_ShapeAdded);
+        }
+
+        private void ActiveDocument_ShapeAdded(Visio.IVShape shape)
+        {
+            // Check if the shape is of a type you are interested in (e.g., a specific shape type)
+            // For example, check if the shape name is "Square"
+            //if (shape.Name == "Square")
+            //{
+            //    // Show the form when the specific shape is added
+                fForm.Show();
+            //}
         }
 
         private void ShapeDetector_Shutdown(object sender, System.EventArgs e)
         {
+            //this.Application.ActiveDocument.ShapeAdded -= ActiveDocument_ShapeAdded;
         }
 
         public static HtmlGraph CreateGraphFromFlowchart(Visio.Application application)
