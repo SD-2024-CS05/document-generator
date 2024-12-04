3b1c9cc394940702fcb76c66fe5720d79ca8aa64
RajaT1910 <123199853+RajaT1910@users.noreply.github.com>
2024-12-03 18:20:01 -0800

    solving issues for page and startend node

--- ShapeHandler/Objects/Graph/FlowchartNode.cs
+++ ShapeHandler/Objects/Graph/FlowchartNode.cs
@@ -13,6 +13,9 @@ namespace ShapeHandler.Objects
         public string Label { get; set; }
         public NodeType Type { get; set; }
 
+        // adding url
+        public string URL {get; set;}
+
         protected FlowchartNode(string label, NodeType type)
         {
             Id = Guid.NewGuid().ToString();
