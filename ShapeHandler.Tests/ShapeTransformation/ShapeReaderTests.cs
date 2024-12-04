45e47988340147ec3014993fe02e71f8335ffbb6
Joseph Holdnak <90223594+jholdnak@users.noreply.github.com>
2024-12-03 17:10:32 -0800

    Test for anglesharp

--- ShapeHandler.Tests/ShapeTransformation/ShapeReaderTests.cs
+++ ShapeHandler.Tests/ShapeTransformation/ShapeReaderTests.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 using Microsoft.Office.Interop.Visio;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -12,13 +15,14 @@ namespace ShapeHandler.Tests.ShapeTransformation
     public class ShapeReaderTests
     {
         [TestMethod()]
+        [Ignore]
         [DataRow("TestShape.vsdx")]
         public void ReadShapeDataTest(string visioFileName)
         {
             // I need to make this test better. I know.
             Mock<Application> mockVisioApp = new Mock<Application>();
             Mock<Documents> mockDocuments = new Mock<Documents>();
-            Mock<Document> mockDocument = new Mock<Document>();
+            Mock<Microsoft.Office.Interop.Visio.Document> mockDocument = new Mock<Microsoft.Office.Interop.Visio.Document>();
             mockVisioApp.Setup(app => app.Documents).Returns(mockDocuments.Object);
             mockDocuments.Setup(docs => docs.Add(It.IsAny<string>())).Returns(mockDocument.Object);
             string visioPath = FileManager.GetFilePath(visioFileName);
@@ -31,6 +35,21 @@ namespace ShapeHandler.Tests.ShapeTransformation
         private static void SetupVisioFile(Application application, string visioPath)
         {
             application.Documents.Add(visioPath);
+        }
+
+        [TestMethod]
+        public void ConvertAngleSharp()
+        {
+            IBrowsingContext context = BrowsingContext.New(Configuration.Default);
+            IDocument document = context.OpenNewAsync().Result;
+            IHtmlInputElement input = document.CreateElement("input") as IHtmlInputElement;
+            input.Type = "number";
+            input.Id = "inputUseless";
+            input.Minimum = "420";
+            input.Maximum = "1337";
+            input.ValueAsNumber = 999; 
+            input.ClassList.Add("form-control");
+            Assert.IsTrue(input.ValueAsNumber >= int.Parse(input.Minimum) && input.ValueAsNumber <= int.Parse(input.Maximum));
         }
     }
 }
