73ccf14e025905fa5f14aa7ea5721267a68495bf
Joshua Venable <70110252+Kiwifan1@users.noreply.github.com>
2025-02-04 17:17:26 -0800

    Context menu for Button (#32)
    * qol changes for release builders
    
    * update tag name
    
    * hopeful fix
    
    * forgot output
    
    * update to avoid conflicts
    
    * update action to not run on draft
    
    * fixing to use shapeId
    
    * removing button
    
    * added example
    
    * update for ambiguity
    
    * review

---
+++ ShapeHandler/Helpers/VisioShapeDataHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Visio = Microsoft.Office.Interop.Visio;
+
+namespace ShapeHandler.Objects
+{
+    public static class VisioShapeDataHelper
+    {
+        private static Visio.Shape _activeShape;
+        public static void AddShapeData(int shapeId, string schema, string label = "Input 1")
+        {
+            _activeShape = Globals.ShapeDetector.Application.ActivePage.Shapes[shapeId];
+            // check first if there is a row available
+            if (_activeShape.get_RowCount((short)Visio.VisSectionIndices.visSectionProp) == 0)
+            {
+                _activeShape.AddRow(
+                    (short)Visio.VisSectionIndices.visSectionProp,
+                    (short)Visio.VisRowIndices.visRowFirst, 
+                    (short)Visio.VisRowTags.visTagDefault);
+            }
+            try
+            {
+                SetShapeData(Visio.VisCellIndices.visCustPropsLabel, label);
+                SetShapeData(Visio.VisCellIndices.visCustPropsValue, schema);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message); 
+            }
+        }
+
+        private static void SetShapeData(Visio.VisCellIndices cellIndex, string value)
+        {
+
+            _activeShape.get_CellsSRC(
+                (short)Visio.VisSectionIndices.visSectionProp,
+                (short)Visio.VisRowIndices.visRowFirst,
+                (short)cellIndex
+            ).FormulaU = FormatVal(value);
+        }
+
+        private static string FormatVal(string val)
+        {
+            return "\"" + val + "\"";
+        }
+    }
+}
