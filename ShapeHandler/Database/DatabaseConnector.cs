2456659d12c7d98d04a3b8ae5dd180b933409b11
Joshua Venable <70110252+Kiwifan1@users.noreply.github.com>
2025-03-02 09:31:26 -0800

    updating connector queries

--- ShapeHandler/Database/DatabaseConnector.cs
+++ ShapeHandler/Database/DatabaseConnector.cs
@@ -14,6 +14,13 @@ namespace ShapeHandler.Database
     {
         private readonly IDriver _driver;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatabaseConnector"/> class with the specified URI, user, and password.
+        /// </summary>
+        /// <param name="uri">The URI of the Neo4j database.</param>
+        /// <param name="user">The username for authentication.</param>
+        /// <param name="password">The password for authentication.</param>
+        /// <exception cref="Exception">Thrown when authentication or connection to the database fails.</exception>
         public DatabaseConnector(string uri, string user, string password)
         {
             var auth = AuthTokens.Basic(user, password);
@@ -30,6 +37,11 @@ namespace ShapeHandler.Database
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatabaseConnector"/> class with the specified driver.
+        /// </summary>
+        /// <param name="driver">The Neo4j driver.</param>
+        /// <exception cref="Exception">Thrown when connection to the database fails.</exception>
         public DatabaseConnector(IDriver driver)
         {
             _driver = driver;
@@ -40,11 +52,21 @@ namespace ShapeHandler.Database
             }
         }
 
+        /// <summary>
+        /// Disposes the database driver.
+        /// </summary>
         public void Dispose()
         {
             _driver?.Dispose();
         }
 
+        /// <summary>
+        /// Writes the HTML graph to the database asynchronously.
+        /// </summary>
+        /// <param name="graph">The HTML graph to write.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a boolean indicating success or failure.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the graph is null.</exception>
+        /// <exception cref="Exception">Thrown when writing the graph to the database fails.</exception>
         public async Task<bool> WriteHtmlGraphAsync(HtmlGraph graph)
         {
             if (graph == null) throw new ArgumentNullException(nameof(graph));
@@ -54,7 +76,7 @@ namespace ShapeHandler.Database
                 var tx = await session.BeginTransactionAsync();
                 try
                 {
-                    await WriteNodes(tx, graph);
+                    await WriteNodesAsync(tx, graph);
 
                     await tx.CommitAsync();
                     return true;
@@ -67,7 +89,12 @@ namespace ShapeHandler.Database
             }
         }
 
-        private async Task WriteNodes(IAsyncTransaction tx, HtmlGraph graph)
+        /// <summary>
+        /// Writes the nodes of the HTML graph to the database asynchronously.
+        /// </summary>
+        /// <param name="tx">The transaction to use.</param>
+        /// <param name="graph">The HTML graph containing the nodes.</param>
+        private async Task WriteNodesAsync(IAsyncTransaction tx, HtmlGraph graph)
         {
             HashSet<FlowchartNode> visited = new HashSet<FlowchartNode>();
             HashSet<FlowchartNode> nodes = graph.GetNodes();
@@ -76,29 +103,41 @@ namespace ShapeHandler.Database
             {
                 if (!visited.Contains(node))
                 {
-                    await DFS_CreateGraph(tx, graph, visited, node);
+                    await DFS_CreateGraphAsync(tx, graph, visited, node);
                 }
             }
         }
 
-        private async Task DFS_CreateGraph(IAsyncTransaction tx, HtmlGraph graph, HashSet<FlowchartNode> visited, FlowchartNode node)
+        /// <summary>
+        /// Performs a depth-first search to create the graph in the database asynchronously.
+        /// </summary>
+        /// <param name="tx">The transaction to use.</param>
+        /// <param name="graph">The HTML graph.</param>
+        /// <param name="visited">The set of visited nodes.</param>
+        /// <param name="node">The current node.</param>
+        private async Task DFS_CreateGraphAsync(IAsyncTransaction tx, HtmlGraph graph, HashSet<FlowchartNode> visited, FlowchartNode node)
         {
             visited.Add(node);
-            await CreateNode(tx, node);
+            await CreateNodeAsync(tx, node);
 
             var neighbors = graph.GetConnectedNodesFrom(node);
             foreach (var neighbor in neighbors.Keys)
             {
                 if (!visited.Contains(neighbor))
                 {
-                    await DFS_CreateGraph(tx, graph, visited, neighbor);
+                    await DFS_CreateGraphAsync(tx, graph, visited, neighbor);
                 }
             }
 
-            await CreateRelationships(tx, graph, node, neighbors);
+            await CreateRelationshipsAsync(tx, graph, node, neighbors);
         }
 
-        private async Task CreateNode(IAsyncTransaction tx, FlowchartNode node)
+        /// <summary>
+        /// Creates a node in the database asynchronously.
+        /// </summary>
+        /// <param name="tx">The transaction to use.</param>
+        /// <param name="node">The node to create.</param>
+        private async Task CreateNodeAsync(IAsyncTransaction tx, FlowchartNode node)
         {
             if (node is HtmlNode htmlNode)
             {
@@ -114,9 +153,9 @@ namespace ShapeHandler.Database
                 }
 
                 await tx.RunAsync(@"
-                    UNWIND $attributes AS attribute
-                    MERGE (n:" + node.Type.ToString() + @" {id: $id})
-                    ON CREATE SET n += attribute, n.Label = $label",
+                        UNWIND $attributes AS attribute
+                        MERGE (n:" + node.Type.ToString() + @" {id: $id})
+                        ON CREATE SET n += attribute, n.Label = $label",
                 new { id = htmlNode.Id, attributes = elementAttributes, label = htmlNode.Label });
             }
             else
@@ -125,14 +164,20 @@ namespace ShapeHandler.Database
                 var nodeDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(nodeJson);
 
                 await tx.RunAsync(@"
-                    MERGE (n:" + node.Type.ToString() + @" {id: $id})
-                    ON CREATE SET n += $nodeObj",
+                        MERGE (n:" + node.Type.ToString() + @" {id: $id})
+                        ON CREATE SET n += $nodeObj",
                     new { id = node.Id, nodeObj = nodeDict });
             }
         }
 
-
-        private async Task CreateRelationships(IAsyncTransaction tx, HtmlGraph graph, FlowchartNode currentNode, Dictionary<FlowchartNode, List<Connection>> neighbors)
+        /// <summary>
+        /// Creates relationships between nodes in the database asynchronously.
+        /// </summary>
+        /// <param name="tx">The transaction to use.</param>
+        /// <param name="graph">The HTML graph.</param>
+        /// <param name="currentNode">The current node.</param>
+        /// <param name="neighbors">The neighbors of the current node.</param>
+        private async Task CreateRelationshipsAsync(IAsyncTransaction tx, HtmlGraph graph, FlowchartNode currentNode, Dictionary<FlowchartNode, List<Connection>> neighbors)
         {
             foreach (var neighbor in neighbors.Keys)
             {
@@ -143,22 +188,16 @@ namespace ShapeHandler.Database
                     var connectionJson = JsonConvert.SerializeObject(connection, new Neo4JSerializer());
                     var connectionDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(connectionJson);
 
-                    var conditions = new List<string>();
-                    if (connection.Conditions != null)
-                    {
-                        conditions.Add(connection.Conditions.ToString());
-                    }
+                    var conditions = connection.Conditions?.ToString() ?? null;
 
                     await tx.RunAsync(@"
-                        MATCH 
-                            (a:" + currentNode.Type.ToString() + @" {id: $sourceId}), 
-                            (b:" + neighbor.Type.ToString() + @" {id: $targetId})
-                        MERGE 
-                            (a)-[r:" + connection.Type.ToString().ToUpper() + @" {Label: COALESCE($connectionObj.label, '')}]->(b)
-                        ON CREATE SET r += $connectionObj
-                        WITH r
-                        UNWIND $conditions AS condition
-                        SET r.Conditions = condition",
+                                MATCH 
+                                    (a:" + currentNode.Type + @" {id: $sourceId}), 
+                                    (b:" + neighbor.Type + @" {id: $targetId})
+                                MERGE 
+                                    (a)-[r:" + connection.Type.ToString().ToUpper() + @" {Label: COALESCE($connectionObj.label, '')}]->(b)
+                                ON CREATE SET r += $connectionObj
+                                SET r.Conditions = $conditions",
                         new
                         {
                             sourceId = currentNode.Id,
