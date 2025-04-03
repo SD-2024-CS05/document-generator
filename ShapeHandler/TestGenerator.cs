f27703494cf1e9a61d7bff6031524429c94ac6f5
Joseph Holdnak <90223594+jholdnak@users.noreply.github.com>
2025-04-02 18:50:21 -0700

    Oops

--- ShapeHandler/TestGenerator.cs
+++ ShapeHandler/TestGenerator.cs
@@ -5,6 +5,7 @@ using ShapeHandler.Database.StartEnd;
 using ShapeHandler.Database;
 using ShapeHandler.Objects;
 using System.Windows.Forms;
+using System;
 
 namespace ShapeHandler
 {
@@ -17,45 +18,41 @@ namespace ShapeHandler
 
         private void EditNodeControl_Click(object sender, RibbonControlEventArgs e)
         {
-            var shapes = Globals.ShapeDetector.Application.ActiveWindow.Selection;
-            Shape shape = null;
-            try
+            Selection selection = Globals.ShapeDetector.Application.ActiveWindow.Selection;
+            if (selection != null && selection.Count == 1)
             {
-                foreach (Shape selection in shapes)
+                Shape shape = selection.PrimaryItem;
+                NodeType shapeType = VisioShapeDataHelper.GetNodeType(shape.ID);
+                switch (shapeType)
                 {
-                    shape = selection;
+                    case NodeType.DataInput:
+                        ShapeDataForm shapeDataForm = new ShapeDataForm(shape.ID);
+                        shapeDataForm.ShowDialog();
+                        break;
+                    case NodeType.StartEnd:
+                        string url = VisioShapeDataHelper.GetShapeData(shape.ID)["URL"].ToString();
+                        StartEndForm startEndForm = new StartEndForm(url);
+                        startEndForm.ShowDialog();
+                        VisioShapeDataHelper.UpdateRow(shape.ID, "URL", startEndForm.URL);
+                        break;
+                    case NodeType.Decision:
+                        DecisionControlsForm decisionControlsForm = new DecisionControlsForm(shape.ID);
+                        decisionControlsForm.ShowDialog();
+                        break;
+                    case NodeType.Page:
+                        url = VisioShapeDataHelper.GetShapeData(shape.ID)["URL"].ToString();
+                        StartEndForm pageUrlForm = new StartEndForm(url);
+                        pageUrlForm.ShowDialog();
+                        VisioShapeDataHelper.UpdateRow(shape.ID, "URL", pageUrlForm.URL); ;
+                        break;
+                    default:
+                        MessageBox.Show("You must select a valid shape.");
+                        break;
                 }
             }
-            catch (System.Exception)
+            else
             {
-                MessageBox.Show("You must select a shape.");
-            }
-            NodeType shapeType = VisioShapeDataHelper.GetNodeType(shape.ID);
-            switch (shapeType)
-            {
-                case NodeType.DataInput:
-                    ShapeDataForm shapeDataForm = new ShapeDataForm(shape.ID);
-                    shapeDataForm.ShowDialog();
-                    break;
-                case NodeType.StartEnd:
-                    string url = VisioShapeDataHelper.GetShapeData(shape.ID)["URL"].ToString();
-                    StartEndForm startEndForm = new StartEndForm(url);
-                    startEndForm.ShowDialog();
-                    VisioShapeDataHelper.UpdateRow(shape.ID, "URL", startEndForm.URL);
-                    break;
-                case NodeType.Decision:
-                    DecisionControlsForm decisionControlsForm = new DecisionControlsForm(shape.ID);
-                    decisionControlsForm.ShowDialog();
-                    break;
-                case NodeType.Page:
-                    url = VisioShapeDataHelper.GetShapeData(shape.ID)["URL"].ToString();
-                    StartEndForm pageUrlForm = new StartEndForm(url);
-                    pageUrlForm.ShowDialog();
-                    VisioShapeDataHelper.UpdateRow(shape.ID, "URL", pageUrlForm.URL); ;
-                    break;
-                default:
-                    MessageBox.Show("You must select a valid shape.");
-                    break;
+                MessageBox.Show("You must select one shape.");
             }
         }
     }
